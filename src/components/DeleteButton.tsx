2851e0cab134834d6118e7a14f521d5dec826aaa
Tarek <tarekdevel41@gmail.com>
2025-05-11 20:50:19 +0300

    confirm delete and loading delete

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,30 +1,72 @@
 "use client";
 
 import { Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "./ui/button";
 import { deleteBlog } from "@/actions/blog.action";
 import { toast } from "sonner";
+import { useState } from "react";
 
 const DeleteButton = ({ blogId }: { blogId: string }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
   const handleDelete = async () => {
+    setIsDeleting(true);
     // Handle delete logic here
-    const data = await deleteBlog(blogId);
-    if (data?.success) {
-      toast.success("Blog deleted successfully");
-    } else {
-      toast.error("Failed to delete blog");
+    try {
+      const data = await deleteBlog(blogId);
+      if (data?.success) {
+        toast.success("Blog deleted successfully");
+      } else {
+        toast.error("Failed to delete blog");
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to delete blog");
+    } finally {
+      setIsDeleting(false);
     }
-    console.log("Delete blog with ID:", blogId);
   };
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={handleDelete}
-      className="cursor-pointer hover:text-destructive duration-100"
-    >
-      <Trash />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="hover:text-destructive duration-100 cursor-pointer"
+        >
+          <Trash />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            blog.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleDelete}
+            disabled={isDeleting}
+            className="bg-destructive text-white hover:bg-destructive/90 cursor-pointer"
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 export default DeleteButton;
